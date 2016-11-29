977459c80e35354645a47798706ac3e4f147e401
Pavel Teodosiev <pavelteodosiev@gmail.com>
2016-11-29 06:17:05 +0200

    Add files via upload

---
+++ Button_Controlled_LED.ino
@@ -0,0 +1,19 @@
+//This code if for Button Controlled Led. Subscribe to my YouTube Channel: TechSpaceCrafter
+
+int led = 12;
+int button = 8;
+void setup(){
+  pinMode(led, OUTPUT);
+  pinMode(button, INPUT_PULLUP); //we should set button as INPUT, and also we should enable the internal pull-up resistor. So we should use INPUT_PULLUP in pinMode() function.
+}
+
+void loop(){
+  int SensorValue = digitalRead(button);
+  if(SensorValue)
+  {
+    digitalWrite(led, HIGH);
+  }
+  else{
+    digitalWrite(led, LOW); 
+  }
+}
